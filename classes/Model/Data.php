ccdcaa7bdc4462320d8d2485948a3f0dfb100c95
Jabrayil <cabi10@mail.ru>
2017-09-10 20:53:45 +0400

    Try to refactor your code and make it more intuitively readable.

---
+++ classes/Model/Data.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ *  Jabrayil Alizada
+ *  jabrayil.alizada@gmail.com
+ *  Copyright (c) 2017.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+require_once "DataInterface.php";
+
+/**
+ * Class Data
+ */
+class Data implements DataInterface {
+
+    /**
+     * @return array
+     */
+    public static function getCategories()
+    {
+        return array(
+            "Telephones" => array(
+                "Alcatel 27",
+                "Apple 6",
+                "Blackberry 8",
+                "HTC 23",
+                "Keneksi 256",
+                "Meizu 354",
+                "Philips 353",
+                "Samsung 7",
+                "Xiaomi 95"
+            ),
+            "Tablets" => array(
+                "Alcatel 34",
+                "Asus 11",
+                "Lenovo 106",
+                "Samsung 30"
+            )
+        );
+    }
+
+
+    /**
+     * @param string $str
+     * Example: Alcatel 27
+     * method will return Alcatel
+     * @return string
+     */
+    public static function getSubcategory($str)
+    {
+        return explode(" ", $str)[0];
+    }
+
+    /**
+     * @param string $str
+     * Example: Alcatel 27
+     * method will return 27
+     * @return string
+     */
+    public static function getPageNum($str)
+    {
+        return explode(" ", $str)[1];
+    }
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once "library/simple_html_dom.php";
+require_once "classes/Model/Data.php";
 include_once "classes/db.php";
 
 //> Turning off the time limit
@@ -7,19 +8,7 @@ set_time_limit(0);
 //< Turning off the time limit
 
 //> Necessary pages
-$pageIndexes = array("Telephones" => array("Alcatel 27",
-                                    "Apple 6",
-                                    "Blackberry 8",
-                                    "HTC 23",
-                                    "Keneksi 256",
-                                    "Meizu 354",
-                                    "Philips 353",
-                                    "Samsung 7",
-                                    "Xiaomi 95"),
-                    "Tablets" => array("Alcatel 34",
-                                    "Asus 11",
-                                    "Lenovo 106",
-                                    "Samsung 30"));
+$pageIndexes = Data::getCategories();
 //< Necessary pages
 
 //> Crating base query
@@ -29,72 +18,79 @@ VALUES ";
 
 foreach ($pageIndexes as $category => $products) {
 
-        for ($i = 0; $i < count($products); $i++) {
+    for ($i = 0; $i < count($products); $i++) {
 
-            //> Taking the page of the model
-            $markAndIndex = explode(" ", $products[$i]);
-            $categoryPage = file_get_html('http://irtelecom.az/catalog/index/' . $markAndIndex[1]);
-            //< Taking the page of the model
+        //> Taking the page of the model
+        $categoryPage = file_get_html('http://irtelecom.az/catalog/index/' . Data::getPageNum($products[$i]));
+        //< Taking the page of the model
 
-            //> Taking all the links of the Products
-            $aTags = $categoryPage->find('a[class=catprev]');
-            //< Taking all the links of the Products
+        //> Taking all the links of the Products
+        $aTags = $categoryPage->find('a[class=catprev]');
+        //< Taking all the links of the Products
 
-            for ($j = 0; $j < count($aTags); $j++) {
+        for ($j = 0; $j < count($aTags); $j++) {
 
-                //> Taking the link of the image of the product and saving it
-                $imgSrc = $aTags[$j]->find('img')[0]->src;
-                if (!file_exists('images/' . basename($imgSrc))) copy($imgSrc, 'images/' . basename($imgSrc));
-                //< Taking the link of the image of the product and saving it
-
-                //> Name of the Product
-                $prodName = $aTags[$j]->find('div[class=catprev_name]')[0]->innertext;
-                //< Name of the Product
-
-                //> Price of the Product
-                $property = 'data-product_price';
-                $prodPrice = $aTags[$j]->$property;
-                //< Price of the Product
-
-                //> Page of the Product
-                $prodPage = file_get_html('http://irtelecom.az'.$aTags[$j]->href);
-                //< Page of the Product
-
-                //> Color of the Product
-                $prodColor = $prodPage->find('span[class=mc_colorname]')[0]->innertext;
-                //< Color of the Product
-
-                //> Property name of the Product
-                $speclabels = $prodPage->find('span[class=f_speclabel]');
-                //< Property name of the Product
-
-                //> Property value of the Product
-                $specvalues = $prodPage->find('span[class=f_specvalue]');
-                //< Property value of the Product
-
-                //> Creating a description of the Product
-                $prodDescr="";
-                for ($k=0;$k<count($speclabels);$k++){
-                    $prodDescr .= $speclabels[$k]->innertext . "  " . $specvalues[$k]->innertext . "\n";
-                }
-                //< Creating a description of the Product
-
-                //> Operating system of the Product
-                $prodOperSys = $specvalues[0]->innertext;
-                if (!empty($prodOperSys)&&strtolower($prodOperSys[0])=='i') $prodOperSys = "iOS";
-                else $prodOperSys = "Android";
-                //< Operating system of the Product
-
-                //> Creating final query
-                $sql .= "('" . addslashes(htmlspecialchars($prodName)) . "',(select id
-from categories
-where category_name='".$category."'),'".addslashes(htmlspecialchars(basename($imgSrc))) . "','" . addslashes(htmlspecialchars($prodPrice)) . "'," . "(select id
-from marks
-where mark_name='".$markAndIndex[0]."'),'".addslashes(htmlspecialchars($prodColor))."','".addslashes(htmlspecialchars($prodDescr))."','".(0.5*rand(0,10))."','".addslashes(htmlspecialchars($prodOperSys))."')";
-                if ($category!=end($pageIndexes)||$i < count($products) - 1 || $j < count($aTags) - 1) $sql .= ',';
-                //< Creating final query
+            //> Taking the link of the image of the product and saving it
+            $imgSrc = $aTags[$j]->find('img')[0]->src;
+            if (!file_exists('images/' . basename($imgSrc))) {
+                copy($imgSrc, 'images/' . basename($imgSrc));
             }
+            //< Taking the link of the image of the product and saving it
+
+            //> Name of the Product
+            $prodName = $aTags[$j]->find('div[class=catprev_name]')[0]->innertext;
+            //< Name of the Product
+
+            //> Price of the Product
+            $property = 'data-product_price';
+            $prodPrice = $aTags[$j]->$property;
+            //< Price of the Product
+
+            //> Page of the Product
+            $prodPage = file_get_html('http://irtelecom.az' . $aTags[$j]->href);
+            //< Page of the Product
+
+            //> Color of the Product
+            $prodColor = $prodPage->find('span[class=mc_colorname]')[0]->innertext;
+            //< Color of the Product
+
+            //> Property name of the Product
+            $speclabels = $prodPage->find('span[class=f_speclabel]');
+            //< Property name of the Product
+
+            //> Property value of the Product
+            $specvalues = $prodPage->find('span[class=f_specvalue]');
+            //< Property value of the Product
+
+            //> Creating a description of the Product
+            $prodDescr = "";
+            for ($k = 0; $k < count($speclabels); $k++) {
+                $prodDescr .= $speclabels[$k]->innertext . "  " . $specvalues[$k]->innertext . "\n";
+            }
+            //< Creating a description of the Product
+
+            //> Operating system of the Product
+            $prodOperSys = $specvalues[0]->innertext;
+            if (!empty($prodOperSys) && strtolower($prodOperSys[0]) == 'i') {
+                $prodOperSys = "iOS";
+            } else {
+                $prodOperSys = "Android";
+            }
+            //< Operating system of the Product
+
+            //> Creating final query
+            $sql .= "('" . addslashes(htmlspecialchars($prodName)) . "',(select id
+from categories
+where category_name='" . $category . "'),'" . addslashes(htmlspecialchars(basename($imgSrc))) . "','" . addslashes(htmlspecialchars($prodPrice)) . "'," . "(select id
+from marks
+where mark_name='" . Data::getSubcategory($products[$i]) . "'),'" . addslashes(htmlspecialchars($prodColor)) . "','" . addslashes(htmlspecialchars($prodDescr)) . "','" . (0.5 * rand(0,
+                        10)) . "','" . addslashes(htmlspecialchars($prodOperSys)) . "')";
+            if ($category != end($pageIndexes) || $i < count($products) - 1 || $j < count($aTags) - 1) {
+                $sql .= ',';
+            }
+            //< Creating final query
         }
+    }
 }
 
 //> Query execution
